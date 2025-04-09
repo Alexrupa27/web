79b19866a0444ca0b4fec6b6d5a171206231eaa5
AlexRupa <Alexrupa123@gmail.com>
2025-04-09 10:40:40 +0200

    assets

--- proyectoweb/src/App.jsx
+++ proyectoweb/src/App.jsx
@@ -1,8 +1,8 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Home'; // Asegúrate de que 'Home' tenga la primera letra en mayúscula
-import Contact from './Contact'; // Asegúrate de que 'Contact' tenga la primera letra en mayúscula
+import Home from './pages/Home'; // Asegúrate de que 'Home' tenga la primera letra en mayúscula
+import Contact from './pages/Contact'; // Asegúrate de que 'Contact' tenga la primera letra en mayúscula
 
 function App() {
   return (
