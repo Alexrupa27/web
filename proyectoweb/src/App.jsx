ef5654cb24c3d5c33a851168d951bac585fe76fa
AlexRupa <Alexrupa123@gmail.com>
2025-04-09 10:35:08 +0200

    config routes

--- proyectoweb/src/App.jsx
+++ proyectoweb/src/App.jsx
@@ -1,31 +1,16 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home'; // Asegúrate de que 'Home' tenga la primera letra en mayúscula
 import Contact from './Contact'; // Asegúrate de que 'Contact' tenga la primera letra en mayúscula
 
 function App() {
   return (
     <Router>
-      <div>
-        {/* Barra de navegación con enlaces */}
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Inicio</Link>
-            </li>
-            <li>
-              <Link to="/Contact">Contacto</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* Definir las rutas usando Routes en lugar de Switch */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Contact" element={<Contact />} />
         </Routes>
-      </div>
     </Router>
   );
 }
