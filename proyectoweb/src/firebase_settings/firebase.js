cd5dd070237a9b4a2f485e77d7ac9ded96ef1b19
Darrelff <valorizan2004@gmail.com>
2025-04-24 12:02:23 +0200

    Update firebase.js

--- proyectoweb/src/firebase_settings/firebase.js
+++ proyectoweb/src/firebase_settings/firebase.js
@@ -3,14 +3,14 @@ import { getDatabase, ref, onValue } from 'firebase/database';
 
 // Tu configuración de Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyAqBgvAAg939Rpe5P3ia9-BNJDb_KhBHU8",
-  authDomain: "tramparatas-a5771.firebaseapp.com",
-  databaseURL: "https://tramparatas-a5771-default-rtdb.firebaseio.com",
-  projectId: "tramparatas-a5771",
-  storageBucket: "tramparatas-a5771.firebasestorage.app",
-  messagingSenderId: "14584722621",
-  appId: "1:14584722621:web:00b3d48ffa7096f08733d4",
-  measurementId: "G-4CWL7N1490"
+  apiKey: "AIzaSyDTAgQkw3EpO08Jpmz1YWMLUej3uQxw424",
+  authDomain: "esp-database-a23e4.firebaseapp.com",
+  databaseURL: "https://esp-database-a23e4-default-rtdb.firebaseio.com",
+  projectId: "esp-database-a23e4",
+  storageBucket: "esp-database-a23e4.firebasestorage.app",
+  messagingSenderId: "548318257448",
+  appId: "1:548318257448:web:539a56abfa7030b4a5701d",
+  measurementId: "G-V127SGS8Q6"
 };
 
 // Inicializa Firebase
