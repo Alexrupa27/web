549a526bf577bcc3860ee44284b6cf8b91182469
AlexRupa <Alexrupa123@gmail.com>
2025-04-30 10:29:18 +0200

    update error

--- proyectoweb/src/pages/Dispositivos.jsx
+++ proyectoweb/src/pages/Dispositivos.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
-import Home from '../components/ConectaDispositivos'; //cambiar
+import ConectaDispositivos from '../components/ConectaDispositivos'; //cambiar
 // import './styles/App.css'; 
 
 function MenuPrincipal() {
   return (
     <div className="MenuPrincipal">
       <Header />
-      <Home />
+      <ConectaDispositivos />
     </div>
   );
 }
